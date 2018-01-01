60077f784e427dabf1c8eee38ca5494443d40b63
Gemma Curl <gcurl@Gemmas-MacBook-Air.local>
2018-01-01 12:26:43 -0500

    Added new event form

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,43 @@
 class EventsController < ApplicationController
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(user_params)
+    if @event.save
+      flash[:success] = "Event created successfully!"
+      redirect_to @event
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @event = Event.find(params[:id])
+  end
+
+  def update
+    @event = User.find(params[:id])
+    if @event.update_attributes(event_params)
+      # Handle a successful update.
+    else
+      render 'edit'
+    end
+  end
+
+  def index
+    @events = Event.all
+  end
+  
+  private
+
+    def event_params
+      params.require(:user).permit(:title, :description, :date_time,
+                                   :address, :price, :website)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   get 'pages/home'
 
   root 'pages#home'
+
+  resources :events
 end
